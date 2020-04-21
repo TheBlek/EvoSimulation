4d00b0013462f58a4979b9ca682ac71eee844a6b
Kolya <zevs00100@yandex.ru>
2020-04-21 21:37:09 +0700

    Empty commit

--- enviroment.py
+++ enviroment.py
@@ -1,22 +1,13 @@
 class Tile:
-
     def __init__(self, ID, x, y):
         self.ID = ID
         self.x = x
         self.y = y
-    
-
-class Enviroment:
-
-    def __init__(self, width, height):
-        self.Tiles = [None] * width * height
-        for i in range(width * height):
-            self.Tiles[i] = Tile(i, int(i / width), i % width)
-
-myEnv = Enviroment(10,10)
 
 class Animal:
-    def __init__(self,x,y,energy):
+    def __init__(self,ID,x,y,energy):
+        self.ID = ID
         self.x = x            
         self.y = y
         self.energy = energy
+        
